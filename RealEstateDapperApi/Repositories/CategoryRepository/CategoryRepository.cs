8bc590bb6e6e3678511e4de1c6e679d80a53d809
elifkoca2 <koca24988@gmail.com>
2024-09-26 00:06:35 +0300

    Add project files.

---
+++ RealEstateDapperApi/Repositories/CategoryRepository/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using RealEstateDapperApi.Dtos.CategoryDtos;
+using RealEstateDapperApi.Models.DapperContext;
+using Dapper;
+namespace RealEstateDapperApi.Repositories.CategoryRepository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly Context _context;
+
+        public CategoryRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            string query = "Select * From Category";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultCategoryDto>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
