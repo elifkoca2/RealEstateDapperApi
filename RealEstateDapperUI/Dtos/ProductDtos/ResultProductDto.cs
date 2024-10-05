ba2b9ec0c36b8b39ef8436ea96aa2ab99e2d2aa7
elifkoca2 <koca24988@gmail.com>
2024-10-06 00:50:03 +0300

    Additions were made to the project

--- RealEstateDapperUI/Dtos/ProductDtos/ResultProductDto.cs
+++ RealEstateDapperUI/Dtos/ProductDtos/ResultProductDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace RealEstateDapperUI.Dtos.ProductDtos
 {
-    public class ResultProductDtos
+    public class ResultProductDto
     {
         public int productID { get; set; }
         public string title { get; set; }
